ec8ed22b7c5c25ef090c36286964e5af6ef8ad37
danmuck <danmuck426@gmail.com>
2024-03-19 19:21:29 -0400

    .gitignore

--- worker.py
+++ worker.py
@@ -1,37 +1,41 @@
 from protocol import Parallel
 
+
 class Worker(Parallel):
-    def __init__(s, address:str,port:int):
-        rpcs  = {"worker-accept": s.worker_accept,
-                 "fetch-module": s.fetch_module,
-                 "fetch-fragment": s.fetch_fragment,
-                 "activate": s.activate}
-        
+    def __init__(s, address: str, port: int):
+        rpcs = {
+            "worker-accept": s.worker_accept,
+            "fetch-module": s.fetch_module,
+            "fetch-fragment": s.fetch_fragment,
+            "activate": s.activate,
+        }
+
         super().__init__(address, port, rpcs)
         s.supervisor = None
 
     def join_network(s, address, port):
-        s.send_message(address,port,"worker-join", None)
+        s.send_message(address, port, "worker-join", None)
 
     # log the supervisor and webserver
-    def worker_accept(s, message:dict):
-            s.supervisor = message['data']['supervisor']
-            print(s.supervisor)
+    def worker_accept(s, message: dict):
+        s.supervisor = message["data"]["supervisor"]
+        print(s.supervisor)
 
     # fetch module for executing tasks
-    def fetch_module(s, message:dict):
+    def fetch_module(s, message: dict):
         # to do
         return
-    
-    def fetch_fragment(s, message:dict):
+
+    def fetch_fragment(s, message: dict):
         # to do
         return
 
     # lock and execute task using provides resources
-    def activate(s, message:dict):
+    def activate(s, message: dict):
         # to do
         return
-    
+
+
 import sys
 import time
 
@@ -41,8 +45,8 @@ if __name__ == "__main__":
     address = sys.argv[1]
     port = int(sys.argv[2])
 
-    test = Worker(address,port)
-    
-    test.join_network("jacob",11030)
+    test = Worker(address, port)
+
+    test.join_network(address, 11030)
     time.sleep(0.5)
     print("supervisor", test.supervisor)
