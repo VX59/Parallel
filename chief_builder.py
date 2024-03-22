31d8e51ab475e2a5b1fc5a4ba2f362aa5502055d
Jacob DeRosa <jderosa3@buffalo.edu>
2024-03-22 19:24:30 -0400

    implementing worker builder and fixing stuff

--- chief_builder.py
+++ chief_builder.py
@@ -1,32 +1,41 @@
-from docker_utils import get_container, create_archive
+from utils import get_container, create_archive, find_free_port
 import socket
-# the chief http server runs in a docker container
+import docker
+import subprocess
+
+# the chief runs in a docker container
 address:str = socket.gethostname()
 port:int = 11030
-httpport:int = 11050
+httpport:int = 11040
 
-container = get_container("Chief", port=port, httpport=httpport)
+name = "chief"
+
+running_containers = docker.from_env().containers.list()
+running_names = list(map(lambda x: x.name, running_containers))
+
+if name in running_names:
+    print(f"restarting {name} container")
+    subprocess.run(["docker", "stop", name])
+else:
+    print(f"cold starting {name} container")
+
+container = get_container(name, port=port, httpport=httpport)
 print("setting up environment...")
+
+# create working directories
 container.exec_run("mkdir -p /app/resources/modules/module_archives", workdir="/")
 container.exec_run("mkdir -p /app/resources/jobs", workdir="/")
 
-# copy the protocol to the container
-with open("protocol.py", "rb") as chief:
-    protocol_buffer:bytes = chief.read()
+# copy the protocol and chief to the container
+for file in ["protocol.py", "chief.py"]:
+    with open(file, "rb") as chief:
+        protocol_buffer:bytes = chief.read()
 
-    protocol_archive:bytes = create_archive(protocol_buffer, "protocol.py")
-    container.put_archive("/app/", protocol_archive)
+        protocol_archive:bytes = create_archive(protocol_buffer, file)
+        container.put_archive("/app/", protocol_archive)
 
-    chief.close()
+        chief.close()
 
-# copy the chief to the container
-with open("chief.py", "rb") as chief:
-    chief_buffer:bytes = chief.read()
-
-    chief_archive:bytes = create_archive(chief_buffer, "chief.py")
-    container.put_archive("/app/", chief_archive)
-
-    chief.close()
-
-print(address+" starting http server on port", httpport) 
-container.exec_run(f"python /app/chief.py {address} {str(port)} {str(httpport)}", workdir="/")
+print(f"{address} is accepting workers at port {port}", )
+print(f"{address}'s http server is at port {httpport}")
+container.exec_run(f"python /app/chief.py {address} {port} {httpport}", workdir="/")
